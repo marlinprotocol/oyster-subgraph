34d0e02991c1ca4345cbbffc1a757c6358ba2e42
Prateek Reddy <prateekreddyy@gmail.com>
2023-04-14 15:52:14 +0400

    feat: add settlement history for jobs

--- src/implementation.ts
+++ src/implementation.ts
@@ -5,7 +5,9 @@ import {
     JobClosed,
     JobDeposited,
     JobWithdrew,
-    JobRevisedRate,
+    JobReviseRateInitiated,
+    JobReviseRateCancelled,
+    JobReviseRateFinalized,
     ProviderAdded,
     ProviderRemoved,
     ProviderUpdatedWithCp
@@ -13,37 +15,42 @@ import {
 
 import { bigInt, BigInt } from "@graphprotocol/graph-ts/common/numbers";
 // Import entity class
-import { Job, Provider } from "../generated/schema";
+import { Job, SettlementHistory, Provider, ReviseRateRequest } from "../generated/schema";
+import { log, store } from "@graphprotocol/graph-ts";
 
 // Job event handlers
 
 export function handleJobOpened(event: JobOpened): void {
     const id = event.params.job.toHex();
-    const job = new Job(id);
+    let job = Job.load(id);
 
+    if(!job) {
+        job = new Job(id);
+    }
+
+    job.live = true;
     job.metadata = event.params.metadata;
     job.owner = event.params.owner;
     job.provider = event.params.provider;
     job.rate = event.params.rate;
     job.balance = event.params.balance;
     job.lastSettled = event.params.timestamp;
-    job.live = true;
+    job.amountPaid = BigInt.zero();
+    job.createdAt = event.params.timestamp;
 
     job.save();
 }
 
 export function handleJobDeposited(event: JobDeposited): void {
     const id = event.params.job.toHex();
-    const job = Job.load(id);
+    let job = Job.load(id);
 
     if (!job) {
-        return
-    } else {
-        if (job.balance) {
-            
-            job.balance = bigInt.plus(job.balance!, event.params.amount);
-        }
+        log.error("Deposited for non existent job id {}", [id]);
+        return;
     }
+    job.balance = job.balance.plus(event.params.amount);
+    job.save();
 }
 
 export function handleJobClosed(event: JobClosed): void {
@@ -51,27 +58,42 @@ export function handleJobClosed(event: JobClosed): void {
     const job = Job.load(id);
 
     if (!job) {
-        return
+        log.error("Closed non existent job with id {}", [id]);
+        return;
     } else {
-        job.metadata = null;
-        job.owner = null;
-        job.provider = null;
-        job.lastSettled = null;
-        job.rate = null;
-        job.balance = null;
-        job.live = false;
+        store.remove("Job", id);
     }
 }
 
-export function handleJobRevisedRate(event: JobRevisedRate): void {
+export function handleJobRevisedRateInitiated(event: JobReviseRateInitiated): void {
+    const id = event.params.job.toHex();
+    let request = ReviseRateRequest.load(id);
+
+    if(!request) {
+        request = new ReviseRateRequest(id);
+    }
+
+    request.jobId = event.params.job;
+    request.value = event.params.newRate;
+    request.save();
+}
+
+export function handleJobRevisedRateCancelled(event: JobReviseRateCancelled): void {
+    store.remove("ReviseRateRequest", event.params.job.toHex());
+}
+
+export function handleJobRevisedRateFinalized(event: JobReviseRateFinalized): void {
     const id = event.params.job.toHex();
     const job = Job.load(id);
 
+    store.remove("ReviseRateRequest", event.params.job.toHex());
+
     if (!job) {
         return
-    } else {
-        job.rate = event.params.newRate;
     }
+    
+    job.rate = event.params.newRate;
+    job.save();
 }
 
 export function handleJobSettled(event: JobSettled): void {
@@ -80,20 +102,30 @@ export function handleJobSettled(event: JobSettled): void {
 
     if (!job) {
         return
+    }
+    const amount = event.params.amount;
+    if (amount.gt(job.balance)) {
+        log.error("Job Settled for amount {} while balance is {}", [amount.toString(), job.balance.toString()]);
+        job.balance = BigInt.zero();
     } else {
-        const amount = event.params.amount;
-        if (!job.balance) {
-            job.balance = BigInt.zero();
-        } else {
-            if (amount.gt(job.balance!)) {
-                job.balance = BigInt.zero();
-            } else {
-                job.balance = job.balance!.minus(amount);
-            }
-        } 
-        
-        job.lastSettled = event.block.timestamp;        
+        job.balance = job.balance.minus(amount);
     } 
+    
+    job.lastSettled = event.block.timestamp;
+    job.save();
+
+    let settlementId = id + event.block.timestamp.toString();
+    let settlement = SettlementHistory.load(settlementId);
+
+    if(settlement) {
+        return;
+    }
+
+    settlement = new SettlementHistory(settlementId);
+    settlement.job = id;
+    settlement.ts = event.block.timestamp;
+    settlement.amount = amount;
+    settlement.save();
 }
 
 export function handleJobWithdrew(event: JobWithdrew): void {
@@ -104,15 +136,12 @@ export function handleJobWithdrew(event: JobWithdrew): void {
         return
     } else {
         const amount = event.params.amount;
-        if (!job.balance) {
+        if (amount.gt(job.balance)) {
             job.balance = BigInt.zero();
         } else {
-            if (amount.gt(job.balance!)) {
-                job.balance = BigInt.zero();
-            } else {
-                job.balance = job.balance!.minus(amount);
-            }
-        }        
+            job.balance = job.balance.minus(amount);
+        }  
+        job.save();      
     } 
 }
 
@@ -134,8 +163,7 @@ export function handleProviderRemoved(event: ProviderRemoved): void {
     if (!provider) {
         return
     } else {
-        provider.cp = null;
-        provider.live = false;
+        store.remove("Provider", id);
     }
 }
 
@@ -147,5 +175,6 @@ export function handleProviderUpdatedWithCp(event: ProviderUpdatedWithCp): void 
         return
     } else {
         provider.cp = event.params.newCp;
+        provider.save();
     }
 }
